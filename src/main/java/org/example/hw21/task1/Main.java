283da837ac84a7c6adeaf1016dad8bf8816b2ee7
cloud9cloud9 <nalezhitiyvlad@gmail.com>
2024-01-10 20:28:41 +0200

    hw21 correction to hw about patterns

--- src/main/java/org/example/hw21/task1/Main.java
+++ src/main/java/org/example/hw21/task1/Main.java
@@ -4,5 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Product product = Product.createProduct("banana", 3, "fruits");
         System.out.println(product);
+
+        Product byCategory = Product.createProductByCategory("vegetable");
+        System.out.println(byCategory);
+        Product byPrice = Product.createProductByPrice(35);
+        System.out.println(byPrice);
+        Product byName = Product.createProductByName("Tomato");
+        System.out.println(byName);
     }
 }
